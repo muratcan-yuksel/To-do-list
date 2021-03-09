87caa367056bf1c018c49ec749c466c252e40fbe
Murat Can Yüksel <muratcanyukselpro@gmail.com>
2021-03-09 11:42:59 +0300

    added a createDOM.js file, I'll work on it more. Also added a add Todo button in the index.html file, I'll use modals on it

---
+++ src/createDOM.js
@@ -0,0 +1,9 @@
+import { Todo } from "./createTodo";
+
+const createDOM = (() => {
+  //get the container Div
+  const container = document.querySelector(".container");
+
+  const title = document.createElement("DIV");
+  title.setAttribute("class", "title");
+})();
