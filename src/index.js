28235874457c8bcbaede8c7f55e42e8cb0f46745
Murat Can Yüksel <muratcanyukselpro@gmail.com>
2021-03-10 16:32:37 +0300

    created a modal.js file and exported it into the index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,9 @@
 import { Todo, newTodo } from "./createTodo";
 import { getInput, addDivs } from "./createDOM";
+import { modal } from "./modal";
+
+//calls the modal logic from modal.js
+modal();
 
 //an array that holds the to-dos
 const TodoList = [
@@ -24,29 +28,3 @@ button.addEventListener("click", function () {
 });
 
 export { container, TodoList };
-
-// Get the modal
-var modal = document.getElementById("myModal");
-
-// Get the button that opens the modal
-var btn = document.getElementById("myBtn");
-
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
-
-// When the user clicks the button, open the modal
-btn.onclick = function () {
-  modal.style.display = "block";
-};
-
-// When the user clicks on <span> (x), close the modal
-span.onclick = function () {
-  modal.style.display = "none";
-};
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function (event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
-};
