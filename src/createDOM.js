445f8f954c36b03435e250a63a6c258fd434823c
Murat Can Yüksel <muratcanyukselpro@gmail.com>
2021-03-11 18:45:58 +0300

    fallback

--- src/createDOM.js
+++ src/createDOM.js
@@ -20,22 +20,26 @@ const getInput = (() => {
 
   return { getTitle, getDetails, getDate, getPriority };
 })();
+
+//these are global bcs I want to use them in more than one function
+//Create elements
+const checkBox = document.createElement("INPUT");
+const title = document.createElement("DIV");
+const details = document.createElement("DIV");
+const date = document.createElement("DIV");
+const priority = document.createElement("DIV");
+const deleteButton = document.createElement("BUTTON");
+//create a todo container
+//the reason to do this is to be able to delete a singular to do element
+const todoContainer = document.createElement("DIV");
+
 //creates Divs and writes the user input into the DOM
 const addDivs = () => {
-  //create a todo container
-  //the reason to do this is to be able to delete a singular to do element
-  const todoContainer = document.createElement("DIV");
   //give it a classname
   todoContainer.setAttribute("class", "todoContainer");
   //append it to the container from index.js
   container().appendChild(todoContainer);
-  //Create elements
-  const checkBox = document.createElement("INPUT");
-  const title = document.createElement("DIV");
-  const details = document.createElement("DIV");
-  const date = document.createElement("DIV");
-  const priority = document.createElement("DIV");
-  const deleteButton = document.createElement("BUTTON");
+
   // set their attributes for identification and append them to the todo container Div
   checkBox.setAttribute("type", "checkbox");
   checkBox.setAttribute("class", "todoCheck");
@@ -63,5 +67,7 @@ const addDivs = () => {
   //the answer is todoContainer
   console.log(deleteButton.parentNode);
 };
+
+const deleteTodo = () => {};
 
 export { getInput, addDivs };
