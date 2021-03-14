a732770db6e806f9af164354bdd80376d659a733
Murat Can Yüksel <muratcanyukselpro@gmail.com>
2021-03-14 20:22:35 +0300

    I have no idea what I did but the problem is fixed??

--- src/createDOM.js
+++ src/createDOM.js
@@ -203,27 +203,18 @@ const addDivsForProjects = () => {
             function initRemoveButton() {
               this.parentNode.parentNode.removeChild(this.parentNode);
               console.log(TodoList);
-              //the below function finds the deleted todo's index in the array, and removes it
-              // I couldn't think of a way to implement this in the creaTodo.js
-              let index = TodoList.findIndex(
-                (obj) =>
-                  obj.title === title.textContent &&
-                  obj.details === details.textContent
-              );
-              //remove that index
-              TodoList.splice(index, 1);
 
               //this function deletes the same element from the created array in projectArray object too
-              for (let project in projectArray) {
-                // console.log("new thing " + projectArray[project]);
-                let index = projectArray[project].findIndex(
-                  (obj) =>
-                    obj.title === title.textContent &&
-                    obj.details === details.textContent
-                );
-                //remove that index
-                projectArray[project].splice(index, 1);
-              }
+
+              // console.log("new thing " + projectArray[project]);
+              let index = projectArray[project].findIndex(
+                (obj) =>
+                  obj.title === projectArray[project][i].title.textContent &&
+                  obj.details === projectArray[project][i].details.textContent
+              );
+              //remove that index
+              projectArray[project].splice(index, 1);
+
               console.log(TodoList);
             }
           })();
