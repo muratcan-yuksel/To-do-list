f37be82930c0795a9203d6da73482994e1056373
Murat Can Yüksel <muratcanyukselpro@gmail.com>
2021-03-14 18:05:30 +0300

    made the same changes with the first algorithm, i.e. the todolist array delete function works fine

--- src/createDOM.js
+++ src/createDOM.js
@@ -90,8 +90,7 @@ const addDivs = () => {
       // I couldn't think of a way to implement this in the creaTodo.js
       let index = TodoList.findIndex(
         (obj) =>
-          obj.title === getInput.getTitle() &&
-          obj.details === getInput.getDetails()
+          obj.title === title.textContent && obj.details === details.textContent
       );
       //remove that index
       TodoList.splice(index, 1);
@@ -101,8 +100,8 @@ const addDivs = () => {
         // console.log("new thing " + projectArray[project]);
         let index = projectArray[project].findIndex(
           (obj) =>
-            obj.title === getInput.getTitle() &&
-            obj.details === getInput.getDetails()
+            obj.title === title.textContent &&
+            obj.details === details.textContent
         );
         //remove that index
         projectArray[project].splice(index, 1);
