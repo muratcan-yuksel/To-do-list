4d57a74d930195e8515a6d26fe86b28491dbe63b
Murat Can Yüksel <muratcanyukselpro@gmail.com>
2021-03-19 22:33:32 +0300

    almost implemented a delete function, and added some comments

--- src/deleteProjects.js
+++ src/deleteProjects.js
@@ -9,11 +9,11 @@ import { addDivsForProjects } from "./addDivsForProjects";
 
 const deleteProjects = () => {
   //get the container of projects on the side bar
-  //const wrapper = document.getElementById("projects");
-
   const projects = document.querySelectorAll(".project");
+  //loop through them
   projects.forEach((sideName) => {
     console.log(sideName.textContent);
+    //if it's not the home button...
     if (sideName.textContent != "Home") {
       //the following function loops through the projectArray object and finds the individual arrays inside of it
       for (let project in projectArray) {
@@ -22,11 +22,35 @@ const deleteProjects = () => {
         //0,1,2 etc. in our situation
         //and projectArray[project] will give the array like [1,2,"a"] etc.
         if (
+          //if the project name on the sidebar and the project (as an array matches) and
+          //if the array is empty
           sideName.textContent === project &&
           projectArray[project].length === 0
         ) {
           //   alert("tru fam");
-        }
+          sideName.addEventListener("click", (event) => {
+            //this one is so that what I create won't repeat itself on each click
+            document
+              .querySelectorAll(".projectDelete")
+              .forEach((e) => e.remove());
+            console.log("side");
+            //create a container
+            const projectDelete = document.createElement("DIV");
+            //give it a classname
+            projectDelete.setAttribute("class", "projectDelete");
+            container().appendChild(projectDelete);
+            projectDelete.textContent = "Delete this project";
+          });
+        } else if (
+          //if the array is not empty, delete the created "delete this project" thingy
+          sideName.textContent === project &&
+          projectArray[project].length > 0
+        )
+          sideName.addEventListener("click", (event) => {
+            document
+              .querySelectorAll(".projectDelete")
+              .forEach((e) => e.remove());
+          });
       }
     }
   });
@@ -47,3 +71,16 @@ const deleteProjects = () => {
 };
 
 export { deleteProjects };
+
+// sideName.addEventListener("click", (event) => {
+//     console.log("side");
+//     //create a container
+//     const projectDeleteContainer = document.createElement("DIV");
+//     //give it a classname
+//     projectDeleteContainer.setAttribute("class", "projectDelete");
+//     //append it to the container from index.js
+//     container().appendChild(projectDeleteContainer);
+//     const projectDelete = document.createElement("DIV");
+//     projectDeleteContainer.appendChild(projectDelete);
+//     projectDelete.textContent = "Delete this project";
+//   });
